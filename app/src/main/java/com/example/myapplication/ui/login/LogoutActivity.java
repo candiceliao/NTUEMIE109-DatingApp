85100f41f7fb5ead1b3264cd63a2bbc1aa816a7c
candiceliao <chiystine2587@gmail.com>
2019-05-28 22:26:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/login/LogoutActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication.ui.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.R;
+
+public class LogoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+
+        Button btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+    }
+}
+
+
