c6c09120b6ac2ca2b70d58590bfddf9a08812d7f
xuyuxiang <32697067+xuyuxiang@users.noreply.github.com>
2019-06-04 21:15:23 +0800

    0604
    about socket

---
+++ NTUEMIE109-DatingApp-master/Socket/src/Client.java
@@ -0,0 +1,56 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Client {
+	 public static void main( String[] args ) throws IOException
+	    {
+	        String host = "";
+	        int port = 5987;
+	        Socket socket = null;
+	        Scanner consoleInput = new Scanner( System.in );
+	        System.out.println("½Ð¿é¤JServerºÝ¦ì§}");
+	        host = consoleInput.nextLine();
+	        try
+	        {
+	            socket = new Socket( host, port );
+	            DataInputStream input = null;
+	            DataOutputStream output = null;
+	            
+	            try
+	            {
+	                input = new DataInputStream( socket.getInputStream() );
+	                output = new DataOutputStream( socket.getOutputStream() );
+	                while ( true )
+	                {
+	                    System.out.println( input.readUTF() );
+	                    break;
+	                }
+	            }
+	            catch ( IOException e )
+	            {
+	            }
+	            finally 
+	            {
+	                if ( input != null )
+	                    input.close();
+	                if ( output != null )
+	                    output.close();
+	            }
+	        }
+	        catch ( IOException e )
+	        {
+	            e.printStackTrace();
+	        }
+	        finally
+	        {
+	            if ( socket != null )
+	                socket.close();
+	            if ( consoleInput != null )
+	                consoleInput.close();
+	        }
+	    }
+}
