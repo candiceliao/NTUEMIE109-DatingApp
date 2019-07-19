cbf74b599f9c4d563afca537de61e9f9bcfa63b2
sarah chiang <sarahjohn0304@gmail.com>
2019-07-19 11:00:38 +0800

    added the function for the nextBtn to change the page

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -1,7 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.interest.InterestActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class SignUpActivity extends AppCompatActivity {
     public void onCreate (Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_sign_up);
+
+        Button interestPageBtn = findViewById(R.id.nextBtn);
+
+        interestPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent interestIntent = new Intent();
+                interestIntent.setClass(SignUpActivity.this, InterestActivity.class);
+                startActivity(interestIntent);
+            }
+        });
     }
 }
