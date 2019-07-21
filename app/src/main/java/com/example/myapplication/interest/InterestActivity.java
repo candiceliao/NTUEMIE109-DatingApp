be7b27de9462e21b08c22f7051b3b3c0d41107b8
sarah chiang <sarahjohn0304@gmail.com>
2019-07-22 01:31:19 +0800

    add lifecycle and upgrade the gradle

--- app/src/main/java/com/example/myapplication/interest/InterestActivity.java
+++ app/src/main/java/com/example/myapplication/interest/InterestActivity.java
@@ -1,9 +1,9 @@
 package com.example.myapplication.interest;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.ActivityInterestBinding;
@@ -21,16 +21,11 @@ public class InterestActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_interest);
 
+        interestViewModel = ViewModelProviders.of(this).get(InterestViewModel.class);
+
         binding.setInterestViewModel(interestViewModel);
 
-//        binding.btnRefresh.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                interestViewModel.refresh();
-//            }
-//        });
-        // 在Xml中綁定了Button的OnClick，故此處不再設OnClickListener
+        //不要儲存Activity/Fragment的內容或context在ViewModel中，會被destroy
 
     }
 }
--- app/src/main/java/com/example/myapplication/interest/InterestViewModel.java
+++ app/src/main/java/com/example/myapplication/interest/InterestViewModel.java
@@ -1,11 +1,12 @@
 package com.example.myapplication.interest;
 
+import android.arch.lifecycle.ViewModel;
 import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
 
 import com.example.myapplication.data.model.InterestModel;
 
-public class InterestViewModel {
+public class InterestViewModel extends ViewModel {
 
     public final ObservableField<String> mData = new ObservableField<>();
 
