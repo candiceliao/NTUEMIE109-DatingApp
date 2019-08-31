993bbf7e1ab80c7ee273a275d70bbb93f823c387
yajuyang <s110513050@stu.ntue.edu.tw>
2019-08-31 11:53:07 +0800

    add Login & SignUpActivity

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.myapplication.interest.InterestActivity;
+import java.util.HashSet;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -14,6 +17,12 @@ public class SignUpActivity extends AppCompatActivity {
     public void onCreate (Bundle savedInstance) {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_sign_up);
+        SharedPreferences spref = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = spref.edit();
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        SharedPreferences setting =
+                getSharedPreferences("atm", MODE_PRIVATE);
+        edUserid.setText(setting.getString("PREF_USERID", ""));
 
         Button interestPageBtn = findViewById(R.id.nextBtn);
 
