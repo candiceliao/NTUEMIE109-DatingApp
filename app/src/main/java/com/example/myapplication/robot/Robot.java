257ce0cf3ecbdbc1f64c17131ac4b30834ab3914
sarah chiang <sarahjohn0304@gmail.com>
2019-10-19 19:38:05 +0800

    fix the problem and change the lib to androidX

--- app/src/main/java/com/example/myapplication/robot/Robot.java
+++ app/src/main/java/com/example/myapplication/robot/Robot.java
@@ -1,7 +1,10 @@
-package com.example.myapplication.user.entity;
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
+package com.example.myapplication.robot;
+//import android.arch.persistence.room.ColumnInfo;
+//import android.arch.persistence.room.Entity;
+//import android.arch.persistence.room.PrimaryKey;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
 
 @Entity(tableName = "robot")
 public class Robot {
