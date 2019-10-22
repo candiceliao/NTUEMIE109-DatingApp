43d90b43d0047b79268df66824b67f70edc9721f
sarah chiang <sarahjohn0304@gmail.com>
2019-10-22 17:02:30 +0800

    change android support to androidX

--- app/src/main/java/com/example/myapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/myapplication/WelcomeActivity.java
@@ -3,7 +3,6 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-//import android.support.v7.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
