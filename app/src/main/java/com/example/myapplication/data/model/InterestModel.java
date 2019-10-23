3ee0a8cca43807968e6673e9b124af359f4a703d
sarah chiang <sarahjohn0304@gmail.com>
2019-10-23 13:37:37 +0800

    fix MVVM of interest

--- app/src/main/java/com/example/myapplication/data/model/InterestModel.java
+++ app/src/main/java/com/example/myapplication/data/model/InterestModel.java
@@ -5,13 +5,7 @@ import android.os.Handler;
 public class InterestModel {
 
     public void retrieveData(final onDataReadyCallback callback) {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                callback.onDataReady("New InterestModel");
-            }
-
-        }, 1500);
+        new Handler().postDelayed(() -> callback.onDataReady("New InterestModel"), 1500);
     }
 
     public interface onDataReadyCallback {
--- app/src/main/java/com/example/myapplication/interest/InterestActivity.java
+++ app/src/main/java/com/example/myapplication/interest/InterestActivity.java
@@ -1,10 +1,5 @@
 package com.example.myapplication.interest;
 
-//import android.arch.lifecycle.ViewModelProviders;
-//import android.databinding.DataBindingUtil;
-//import android.os.Bundle;
-//import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,20 +11,20 @@ import com.example.myapplication.databinding.ActivityInterestBinding;
 
 public class InterestActivity extends AppCompatActivity {
 
-    ActivityInterestBinding binding;
+    private ActivityInterestBinding activityInterestBinding;
 
-    InterestViewModel interestViewModel = new InterestViewModel();
+    private InterestViewModel interestViewModel = new InterestViewModel();
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
 
         super.onCreate(saveInstanceState);
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_interest);
+        activityInterestBinding = DataBindingUtil.setContentView(this, R.layout.activity_interest);
 
         interestViewModel = ViewModelProviders.of(this).get(InterestViewModel.class);
 
-        binding.setInterestViewModel(interestViewModel);
+        activityInterestBinding.setInterestViewModel(interestViewModel);
 
         //不要儲存Activity/Fragment的內容或context在ViewModel中，會被destroy
 
