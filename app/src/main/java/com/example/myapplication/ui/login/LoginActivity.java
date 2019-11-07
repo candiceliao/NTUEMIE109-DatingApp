499f0a12d9cdbd0d6dbde85c9ff3a6d8ae2faad3
sarah chiang <sarahjohn0304@gmail.com>
2019-11-07 15:45:39 +0800

    replace with lamda

--- app/src/main/java/com/example/myapplication/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/ui/login/LoginActivity.java
@@ -158,24 +158,21 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-
+        loginViewModel.getLoginResult().observe(this, loginResult -> {
+            if (loginResult == null) {
+                return;
             }
+            loadingProgressBar.setVisibility(View.GONE);
+            if (loginResult.getError() != null) {
+                showLoginFailed(loginResult.getError());
+            }
+            if (loginResult.getSuccess() != null) {
+                updateUiWithUser(loginResult.getSuccess());
+            }
+            setResult(Activity.RESULT_OK);
+
+            //Complete and destroy login activity once successful
+
         });
 
         afterTextChangedListener = new TextWatcher() {
@@ -197,16 +194,12 @@ public class LoginActivity extends AppCompatActivity {
         };
         edUserid.addTextChangedListener(afterTextChangedListener);
         edPasswd.addTextChangedListener(afterTextChangedListener);
-        edPasswd.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(edUserid.getText().toString(),
-                            edPasswd.getText().toString());
-                }
-                return false;
+        edPasswd.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                loginViewModel.login(edUserid.getText().toString(),
+                        edPasswd.getText().toString());
             }
+            return false;
         });
 
         loginBtn = findViewById(R.id.loginBtn);
