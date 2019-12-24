8f5e11e8d6eaaa35e62628e5d76dddc2ce3c9b40
sarah chiang <sarahjohn0304@gmail.com>
2019-12-25 02:27:32 +0800

    add friend page into the nav

---
+++ app/src/main/java/com/example/myapplication/friend/FriendFragment.java
@@ -0,0 +1,22 @@
+package com.example.myapplication.friend;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+
+public class FriendFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        View root = inflater.inflate(R.layout.fragment_friend_page, container, false);
+        return root;
+    }
+}
